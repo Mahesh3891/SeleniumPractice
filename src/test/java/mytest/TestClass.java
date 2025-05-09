3855159d086caf544781271e1391ba6083d3dcf3
sandh <sandh@SweetHome>
2025-05-08 21:25:21 -0400

    Selenium: Verify product title "Combination Pliers" on product page

---
+++ src/test/java/mytest/TestClass.java
@@ -0,0 +1,35 @@
+package mytest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+
+	public static void main(String[] args) throws InterruptedException {
+
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://practicesoftwaretesting.com/");
+		Thread.sleep(3000);
+		
+		WebElement combinationPlierEle = driver.findElement(By.xpath("/html/body/app-root/div/app-overview/div[3]/div[2]/div[1]/a[1]/div[2]/h5"));
+		combinationPlierEle.click();
+		Thread.sleep(3000);
+		
+		WebElement productHeading = driver.findElement(By.xpath("/html/body/app-root/div/app-detail/div[1]/div[2]/h1"));
+		
+		String headingText = productHeading.getText();
+			System.out.println(headingText);
+			
+		if (headingText.equals("Combination Pliers")) {
+			System.out.println("The Heading title is correct, Test Case Passed");
+		}
+		else {
+			System.out.println("The Heading title is incorrect, Test Case Failed");
+		}
+		
+		driver.quit();
+	}
+}
